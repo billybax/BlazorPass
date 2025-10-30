1a12bf7b3554303a135c321b19570eef751ee5e4
iv.bakulin <iv.bakulin@gmail.com>
2025-10-30 11:19:43 +0000

    firebase change

--- BlazorPass/Pages/LocPass.cshtml.cs
+++ BlazorPass/Pages/LocPass.cshtml.cs
@@ -7,43 +7,43 @@ using Microsoft.EntityFrameworkCore;
 
 public class LocPassModel : PageModel
 {
-	private readonly ApplicationDbContext _db;
-	private readonly IHubContext<TableUpdateHub> _hubContext;
-	private readonly PasswordEntryService _passwordService;
-	public List<LocPass> PasswordEntries { get; set; }
+    private readonly ApplicationDbContext _db;
+    private readonly IHubContext<TableUpdateHub> _hubContext;
+    private readonly PasswordEntryService _passwordService;
+    public List<LocPass> PasswordEntries { get; set; }
 
-	public LocPassModel(ApplicationDbContext db, IHubContext<TableUpdateHub> hubContext, PasswordEntryService passwordService)
-	{
-		_db = db;
-		_hubContext = hubContext;
-		_passwordService = passwordService;
-	}
+    public LocPassModel(ApplicationDbContext db, IHubContext<TableUpdateHub> hubContext, PasswordEntryService passwordService)
+    {
+        _db = db;
+        _hubContext = hubContext;
+        _passwordService = passwordService;
+    }
 
-	public LocPass[]? Passes { get; set; }
+    public LocPass[]? Passes { get; set; }
 
-	public async Task OnGetAsync()
-	{
-		PasswordEntries = await _passwordService.GetPasswordEntriesAsync();
-		Passes = await _db.LocPasses.OrderBy(l => l.ResName).ToArrayAsync();
-	}
+    public async Task OnGetAsync()
+    {
+        PasswordEntries = await _passwordService.GetPasswordEntriesAsync();
+        Passes = await _db.LocPasses.OrderBy(l => l.ResName).ToArrayAsync();
+    }
 
-	public async Task<IActionResult> OnPostAddAsync(LocPass entry)
-	{
-		await _passwordService.AddPasswordEntryAsync(entry);
+    public async Task<PartialViewResult> OnGetTableDataAsync()
+    {
+        var passes = await _db.LocPasses.OrderBy(l => l.ResName).ToArrayAsync();
+        return Partial("Shared/_PassTablePartial", passes);
+    }
 
-		// Уведомляем ВСЕХ клиентов об изменении
-		await _hubContext.Clients.All.SendAsync("RefreshTable");
+    public async Task<IActionResult> OnPostAddAsync(LocPass entry)
+    {
+        await _passwordService.AddPasswordEntryAsync(entry);
+        await _hubContext.Clients.All.SendAsync("RefreshTable");
+        return new OkResult();
+    }
 
-		return RedirectToPage();
-	}
-
-	public async Task<IActionResult> OnPostDeleteAsync(int id)
-	{
-		await _passwordService.DeletePasswordEntryAsync(id);
-
-		// Уведомляем ВСЕХ клиентов об изменении
-		await _hubContext.Clients.All.SendAsync("RefreshTable");
-
-		return RedirectToPage();
-	}
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        await _passwordService.DeletePasswordEntryAsync(id);
+        await _hubContext.Clients.All.SendAsync("RefreshTable");
+        return new OkResult();
+    }
 }
