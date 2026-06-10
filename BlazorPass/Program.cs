86df70fbe6f4073bbbfa3c2e375023226e4f9f60
billybax <billybax@yandex.ru>
2026-06-10 22:51:27 +0300

    John Rexton

--- BlazorPass/Program.cs
+++ BlazorPass/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error");
-	// The default HSTS value is30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	// The default HSTS value is30 days. You may want to change this for production scenarios1, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
 app.UseHttpsRedirection();
